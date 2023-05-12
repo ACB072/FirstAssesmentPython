4e1456140b2731a611599a37949057c37de8dbfa
ACB072 <antoniomiralless@gmail.com>
2023-05-12 09:39:56 +0200

    Ejercicios en ficheros

---
+++ Main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__,template_folder='html')
+
+@app.route("/")
+def alimentos():
+    return render_template("start_alimentos.html")
+
+@app.route("/alimentos", methods=['POST'])
+def mostrar_alimentos():
+ # Obtener la alimento seleccionada por el usuario
+
+ # Insertar el código aquí
+        
+ # Renderizar la página de alimentos con el alimento seleccionado
+ return render_template("alimentos.html", alimento=alimento_ingresado)
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
+```
+
+
